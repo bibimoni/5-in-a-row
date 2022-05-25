9ae94e5024e1220696a68573af335a47ee5470d5
dang nguyen bao <proquadi66@gmail.com>
2022-05-25 13:09:32 +0700

    update

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
 const canvas = document.querySelector('canvas');
 
 const c = canvas.getContext('2d');
-
+let mouseCurrentPosition = {x: 0, y: 0};
 //resize canvas
 const GRID_WIDTH = canvas.width = 800;
 const GRID_HEIGHT = canvas.height = 800;
+const PATTERN = 3;
+const winLine = 5;
+
 
 c.fillRect(0, 0,GRID_WIDTH, GRID_HEIGHT);
 let board = new Board({
-    boardWidth: 20, 
-    boardHeight: 20,
-})
-let player1 = new Player({
-    win: false,
+    boardWidth: PATTERN, 
+    boardHeight: PATTERN,
     imageSrc: './img/XO.png',
-    firstTurn: true,
-    icon: 'x'
+    requireLineLength: winLine
 })
-let player2 = new Player({
-    win: false,
-    imageSrc: './img/XO.png',
-    firstTurn: false,
-    icon: 'o'
-})
+
 board.initializeBoard();
 board.drawBoard();
+
+let player1 = new Player({
+    win: false,
+    firstTurn: true,
+    id: 1,
+    boardWidth: PATTERN, 
+    boardHeight: PATTERN,
+})
+
+let player2 = new Player({
+    win: false,
+    firstTurn: false,
+    id: 2,
+    boardWidth: PATTERN, 
+    boardHeight: PATTERN,
+})
+
+const getMousePosition = (canvas, event) => {
+    let rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left - 4; // 4 is the border
+    let y = event.clientY - rect.top - 4;
+    return { x: x, y: y };
+}
+
+
+animationLoop();
+function animationLoop() {
+    window.requestAnimationFrame(animationLoop);
+    board.update();
+    if(board.drawing === true) changeHTML({text : 'TIE'})
+    else if(player1.isWinning === true) changeHTML({text : 'X wins'});
+    else if(player2.isWinning === true) changeHTML({text : 'O wins'});
+}
+
+canvas.addEventListener('mousedown', event => {
+    
+    mouseCurrentPosition =  getMousePosition(canvas, event);
+    if(player1.canPlace){
+        player1.performAction();
+        if(player1.placeTieSuccess) {
+            player1.canPlace = false;
+            player2.canPlace = true;
+            player1.placeTieSuccess = false;
+        }
+    }
+    else if(player2.canPlace){
+        player2.performAction();
+        if(player2.placeTieSuccess) {
+            player2.canPlace = false;
+            player1.canPlace = true;
+            player2.placeTieSuccess = false;
+        }
+    }
+});
 
 
--- src/classes.js
+++ src/classes.js
@@ -1,45 +1,184 @@
-class Player{
-    
-    constructor({win, imageSrc, firstTurn, icon}) {
-        this.win = win;
-        this.image = new Image();
-        this.image.src = imageSrc;
-        this.firstTurn = firstTurn;
-        this.pos = [];
-        this.icon = icon; // x / o
-        this.canPlace = firstTurn // if the first turn is available, the player can place X/O
-
-    } 
-    performAction() {
-        if(!this.canPlace) return;
-
-    }
-    translateMousePosition() {
-
-    }
-}
-
 class Board {
-    constructor({boardWidth, boardHeight}) {
+
+    constructor({ boardWidth, boardHeight, imageSrc, requireLineLength }) {
+        const K = 0.29 / 200;
         this.width = boardWidth;
         this.height = boardHeight;
-        //this.image = new Image();
-        //this.image.src = imageSrc;
-        this.Board; //board is an 2D array
+        this.image = new Image();
+        this.image.src = imageSrc;
+        this.Board = new Array(this.width).fill(0); //board is an 2D array
+        //scale = K * 1/number of tie * pixel of the board
+        this.scale = K * (1 / this.width) * GRID_WIDTH; // 0,29 is suitable for 600 by 600 GRID with 3 ties
+        this.currentImage = 0;
+        this.requireLineLength = requireLineLength;
+        this.drawing = false;
+        //2 end point if there is a 5 in a row
+        this.win_r1 = -1; this.win_r2 = -1; this.win_c1 = -1; this.win_c2 = -1;
     }
 
     initializeBoard() {
-        this.Board = new Array(this.width).fill(0);
-        for(let i = 0; i < this.width; i++) {
+        // 2D array, 1 represents x, 2 represents o, 0 doesn't represent anything
+        // so we need to fill the array with 0 to start  
+        for (let i = 0; i < this.width; i++) {
             this.Board[i] = new Array(this.height).fill(0);
-        }             
+        }
     }
-    drawBoard() {       
-        for(let i = 0; i < this.width; i++) {      
-            for(let j = 0; j < this.height; j++) {
-                (i+j) % 2 === 0 ? c.fillStyle = '#635170' : c.fillStyle = 'pink' ;
-                c.fillRect(i*GRID_WIDTH/this.width, j*GRID_HEIGHT/this.height, GRID_HEIGHT/this.height, GRID_HEIGHT/this.height)
+    drawBoard() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                (i + j) % 2 === 0 ? c.fillStyle = '#635170' : c.fillStyle = 'pink';
+                c.fillRect(i * GRID_WIDTH / this.width, j * GRID_HEIGHT / this.height, GRID_HEIGHT / this.height, GRID_HEIGHT / this.height)
             }
         }
-    }   
+    }
+    draw() {
+        const boardX = GRID_WIDTH;
+        const boardY = GRID_HEIGHT;
+
+        //set the ratio between default grid value 600 and current grid value 
+        const K = GRID_WIDTH / 600;
+
+        // this is the different X-offset that will 
+        //be added to the draw function specific for the O tie
+        //making it fits nicely on the board 
+        let AdjustedOffsetForTheOTie = 56 * (1 / this.width) * K;
+
+        const defaultOffset = 24;
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                // when the indexes is 0, 
+                //dont draw anything or set the frame on the current image to nothing
+                if (this.Board[i][j] === 0) this.currentImage = 2;
+                //the X tie
+                else
+                    if (this.Board[i][j] === 1) this.currentImage = 1;
+                    //the O tie    
+                    else
+                        if (this.Board[i][j] === 2) this.currentImage = 0;
+                c.drawImage(this.image,
+                    this.currentImage * (this.image.width / 2),
+                    0,
+                    (this.image.width / 2),
+                    this.image.height,
+                    //x,y coordinates is determined by the d efault 
+                    //offset for 600 x 600, 3 ties * 1/tie * pixel of the board
+                    //plus the coordinates of the current tie which is calculated 
+                    //by multiplying the index of the board with (pixel of the board / number of ties)
+                    i * (boardX / this.width) + defaultOffset * (1 / this.width) * K + ((this.currentImage !== 0) ? 0 : AdjustedOffsetForTheOTie),
+                    j * (boardY / this.height) + defaultOffset * (1 / this.height) * K,
+                    (this.image.width) * this.scale,
+                    this.image.width * this.scale
+                )
+            }
+        }
+    }
+    checkWin(playerId, lastMove) {
+        
+        if(this.countMove(playerId, lastMove.x, lastMove.y, 1, 0) >= 5) return true;
+        if(this.countMove(playerId, lastMove.x, lastMove.y, 0, 1) >= 5) return true;
+        if(this.countMove(playerId, lastMove.x, lastMove.y, 1, 1) >= 5) return true;
+        if(this.countMove(playerId, lastMove.x, lastMove.y, 1, -1) >= 5) return true;
+
+        this.win_r1 = -1; this.win_r2 = -1; this.win_c1 = -1; this.win_c2 = -1;
+        return false;
+        
+    }
+    //count the length of a player's row
+    countMove(playerId, lastMoveRow, lastMoveColumn, directionRow, directionColumn) {
+        let currentLine = 1;
+
+        let r = lastMoveRow + directionRow; 
+        let c = lastMoveColumn + directionColumn;        
+        while(this.legalSquare(r, c) && this.Board[r][c] === playerId) {
+            currentLine++;
+            r += directionRow;
+            c +=directionColumn;
+        }
+        //set the end point to the last checked square
+        this.win_r1 = r - directionRow; this.win_c1 = c - directionColumn;
+
+        r = lastMoveRow - directionRow;
+        c = lastMoveColumn - directionColumn;
+        while(this.legalSquare(r, c) && this.Board[r][c] === playerId) {
+            currentLine++;
+            r -= directionRow;
+            c -= directionColumn;
+        }
+        //the other end
+        this.win_r2 = r - directionRow; this.win_c2 = c - directionColumn;
+
+        return currentLine;
+    }
+    //check draw method by using the 0 element of the board array
+    checkDraw() {
+        for(let i = 0; i < this.width; i++) 
+            for(let j = 0; j < this.height; j++) {
+                if(this.Board[i][j] === 0) {
+                    this.drawing = false;
+                    return;
+                }
+            }
+        this.drawing = true;
+    }
+
+    legalSquare(r, c) {
+        return r < this.width && c < this.width && r >= 0 && c >= 1;
+    }
+    update() {
+        this.draw();
+        this.checkDraw();
+    }
+}
+class Player {
+
+    constructor({ win, firstTurn, id, boardWidth, boardHeight }) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        this.win = win;
+        this.canPlace = firstTurn; // if the first turn is available, the player can place X/O
+        this.id = id
+        this.translatedMousePosition = { x: 0, y: 0 };
+        this.placeTieSuccess = false;
+        this.moves = [];
+        this.lastMove = undefined;
+        this.isWinning = false;
+    }
+    performAction() {
+        if(this.isWinning === false && board.drawing === false) {
+            this.updateBoard();
+            this.isWinning = board.checkWin(this.id, this.lastMove);
+            //if player wins, stop the game
+        }
+    }
+    // taking mouse position and translate it into indexes of the board
+    mouseBoardPosition() {
+        let translated = { x: 0, y: 0 }
+        let boardX = GRID_WIDTH;
+        let boardY = GRID_HEIGHT;
+        for (let i = 0; i < this.boardWidth; i++) {
+            if (mouseCurrentPosition.x >= i * (boardX / this.boardWidth) && mouseCurrentPosition.x < (i + 1) * (boardX / this.boardWidth)) {
+                translated.x = i;
+            }
+        }
+        for (let i = 0; i < this.boardHeight; i++) {
+            if (mouseCurrentPosition.y >= i * (boardY / this.boardHeight) && mouseCurrentPosition.y < (i + 1) * (boardY / this.boardHeight)) {
+                translated.y = i;
+            }
+        }
+        return translated
+    }
+    updateBoard() {
+        if (board.Board[this.mouseBoardPosition().x][this.mouseBoardPosition().y] === 0 
+            && this.isWinning === false 
+            && board.drawing === false) {
+            board.Board[this.mouseBoardPosition().x][this.mouseBoardPosition().y] = this.id;
+            this.placeTieSuccess = true;
+            //push the success move into the moves array
+            this.lastMove = {
+                x: this.mouseBoardPosition().x,
+                y: this.mouseBoardPosition().y
+            }
+            this.moves.push(this.lastMove);
+        }
+    }
 }
